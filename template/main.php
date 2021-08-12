b2ac8c9008d777fc7c1988b229efbf542e80dc2f
stigmat <56791300+stigmatdev@users.noreply.github.com>
2021-08-12 17:01:19 +0300

    fix layout

--- template/main.php
+++ template/main.php
@@ -7,7 +7,7 @@
 
   for ($i = 0; $i < count($result); $i++) {
     $out .= '<div class="col-sm-4 col-md-4" style="margin-bottom: 50px;">';
-    $out .= '<div class="card text-center rounded">';
+    $out .= '<div class="card text-center rounded myBox">';
     $out .= '<figure style="margin-top: 20px;">';
     $out .= '<img class="img-fluid myImg rounded-circle" src="/static/images/' . $result[$i]['image'] . '">';
     $out .= '</figure>';
